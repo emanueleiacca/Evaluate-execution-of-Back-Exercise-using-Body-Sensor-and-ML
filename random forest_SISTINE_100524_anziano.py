d12697af84eb4b4bb19180de8792ef8b6962f2b1
EmanueleIaccarino <emanueleiaccarino.ei@gmail.com>
2024-05-16 17:07:57 +0200

    codice Ele sistemato

--- random forest_SISTINE_100524_anziano.py
+++ random forest_SISTINE_100524_anziano.py
@@ -1,129 +1,76 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 15 09:37:44 2024
-
-@author: ele99
-"""
-
-
 from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 
-
-# Carica i dati di addestramento
+# Training
 train_file_path = r"AF3_FeatReduc.xlsx"
 df_train = pd.read_excel(train_file_path)
 
-# Estrai le variabili indipendenti (X) e la variabile dipendente (y) dai dati di addestramento
+# Estrai X e y da Train
 X_train = df_train.iloc[:, 2:-1]
 y_train = df_train["Type"]
 subjects_train = df_train["Sub"]
 
-# Carica i dati di test (un singolo soggetto)
+# Test
 test_file_path = r"AF3_A016.xlsx"
 df_test = pd.read_excel(test_file_path)
 
-# Estrai le variabili indipendenti (X) e la variabile dipendente (y) dai dati di test
-X_test = df_test.iloc[:,:-1]
+# Estrai X e y da Test
+X_test = df_test.iloc[:, :-1]
 y_test = df_test["Type"]
 
-# Definisci il modello
+# modello
 model = RandomForestClassifier(n_estimators=30)
-# Inizializza l'oggetto Leave-One-Group-Out Cross-Validation (LOGOCV)
+
+# LOGOCV ma usare GroupCv credo sia piu' efficiente
 logo = LeaveOneGroupOut()
-# Inizializza una lista per memorizzare i punteggi di accuratezza
+
+# lista vuota dove vanno punteggi
 scores = []
-# Esegui la Leave-One-Group-Out Cross-Validation
-for train_index, test_index in logo.split(X_train, y_test, subjects_train):
-    # Dividi i dati in set di addestramento e test per il fold corrente
+
+# CV
+for train_index, test_index in logo.split(X_train, y_train, groups=subjects_train):
+    # Split CV
     X_train_fold, X_test_fold = X_train.iloc[train_index], X_train.iloc[test_index]
-   
-    mean = np.mean(X_train_fold)
-    std_dev = np.std(X_train_fold)
-    X_train_fold_n, MU, SIGMA = (X_train_fold-X_train_fold.mean())/X_train_fold.std(), X_train_fold.mean(), X_train_fold.std() 
-    # Normalizza X train
-   
+    y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
+    
+    # Normalizza dati
+    MU = X_train_fold.mean()
+    SIGMA = X_train_fold.std()
+    X_train_fold_n = (X_train_fold - MU) / SIGMA
     X_test_fold_n = (X_test_fold - MU) / SIGMA
 
-    y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
-    # Addestra il modello sul set di addestramento
+    # Train 
     model.fit(X_train_fold_n, y_train_fold)
-    # Esegui le predizioni sul set di test
-    y_pred = model.predict(X_test_fold_n)
-    # Calcola l'accuratezza sul set di test e aggiungila alla lista dei punteggi
-    accuracy = accuracy_score(y_test_fold, y_pred)
+    
+    # predictions su train
+    y_pred_fold = model.predict(X_test_fold_n)
+    
+    # accuracy sul train
+    accuracy = accuracy_score(y_test_fold, y_pred_fold)
     scores.append(accuracy)
-# Calcola l'accuratezza media su tutti i gruppi
-mean_accuracy = sum(scores) / len(scores)
-print("Mean Accuracy:", mean_accuracy)
-print("Scores:", scores)
-print(np.mean(MU))
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 15 09:37:44 2024
 
-@author: ele99
-"""
+# ean accuracy 
+mean_accuracy = np.mean(scores)
+print("Mean Accuracy from LOGO-CV:", mean_accuracy)
+print("Individual Scores from LOGO-CV:", scores)
 
+# Normalizza il train set
+MU = X_train.mean()
+SIGMA = X_train.std()
+X_train_n = (X_train - MU) / SIGMA
 
-from sklearn.model_selection import LeaveOneGroupOut
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import pandas as pd
-import numpy as np
+# Train 
+model.fit(X_train_n, y_train)
 
+# Normalizza anche il test con stesso metodo
+X_test_n = (X_test - MU) / SIGMA
 
-# Carica i dati di addestramento
-train_file_path = r"C:\Users\ele99\OneDrive\Documenti\Tirocinio\DATA_propotipo\AF3_FeatReduc.xlsx"
-df_train = pd.read_excel(train_file_path)
+# predictions su test
+y_pred_test = model.predict(X_test_n)
 
-# Estrai le variabili indipendenti (X) e la variabile dipendente (y) dai dati di addestramento
-X_train = df_train.iloc[:, 2:-1]
-y_train = df_train["Type"]
-subjects_train = df_train["Sub"]
-
-# Carica i dati di test (un singolo soggetto)
-test_file_path = r"C:\Users\ele99\OneDrive\Documenti\Tirocinio\DATA_propotipo\AF3_A016.xlsx"
-df_test = pd.read_excel(test_file_path)
-
-# Estrai le variabili indipendenti (X) e la variabile dipendente (y) dai dati di test
-X_test = df_test.iloc[:,:-1]
-y_test = df_test["Type"]
-
-# Definisci il modello
-model = RandomForestClassifier(n_estimators=30)
-# Inizializza l'oggetto Leave-One-Group-Out Cross-Validation (LOGOCV)
-logo = LeaveOneGroupOut()
-# Inizializza una lista per memorizzare i punteggi di accuratezza
-scores = []
-# Esegui la Leave-One-Group-Out Cross-Validation
-for train_index, test_index in logo.split(X_train, y_test, subjects_train):
-    # Dividi i dati in set di addestramento e test per il fold corrente
-    X_train_fold, X_test_fold = X_train.iloc[train_index], X_train.iloc[test_index]
-   
-    mean = np.mean(X_train_fold)
-    std_dev = np.std(X_train_fold)
-    X_train_fold_n, MU, SIGMA = (X_train_fold-X_train_fold.mean())/X_train_fold.std(), X_train_fold.mean(), X_train_fold.std() 
-    # Normalizza X train
-   
-    X_test_fold_n = (X_test_fold - MU) / SIGMA
-
-    y_train_fold, y_test_fold = y_train.iloc[train_index], y_train.iloc[test_index]
-    # Addestra il modello sul set di addestramento
-    model.fit(X_train_fold_n, y_train_fold)
-    # Esegui le predizioni sul set di test
-    y_pred = model.predict(X_test_fold_n)
-    # Calcola l'accuratezza sul set di test e aggiungila alla lista dei punteggi
-    accuracy = accuracy_score(y_test_fold, y_pred)
-    scores.append(accuracy)
-# Calcola l'accuratezza media su tutti i gruppi
-mean_accuracy = sum(scores) / len(scores)
-print("Mean Accuracy:", mean_accuracy)
-print("Scores:", scores)
-print(np.mean(MU))
->>>>>>> e18f4167fce6319a3e7cf2348995a16a81eae0d6
+#  accuracy su test 
+test_accuracy = accuracy_score(y_test, y_pred_test)
+print("Accuracy on test data:", test_accuracy)
